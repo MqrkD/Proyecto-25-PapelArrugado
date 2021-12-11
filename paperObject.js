c9a1c05a97bc95d318840893733ac412c23ac8c0
MqrkD <91572258+MqrkD@users.noreply.github.com>
2021-12-10 18:09:03 -0600

    Add files via upload

--- paperObject.js
+++ paperObject.js
@@ -6,21 +6,20 @@ class paperObject {
             friction: 0,
             density: 1.2
         }
+		this.x=x;
 		this.y=y;
 		this.r=r;
 		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options);
 		World.add(world, this.body);
 	}
 	display()	{
-			var rubberpos=this.body.position;		
-			push();
+			var rubberpos=this.body.position;
+			push();		
 			translate(rubberpos.x, rubberpos.y);
+			imageMode(CENTER);
+			image(this.image, 0, 0, this.r);
 			rectMode(CENTER)
-			strokeWeight(4);
-			stroke("black");
-			fill("gray");
-			ellipse(0, 0, this.r, this.r);
-
+			//ellipse(0, 0, this.r, this.r);
 			pop();
 	}
 };
